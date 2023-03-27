00cc51798ddcf2c34fa22666dc864b7b9ad2cf01
Pin-ngam <94055991+findmelily@users.noreply.github.com>
2023-03-27 14:56:04 +0700

    2 commands

--- MyProject2.java
+++ MyProject2.java
@@ -3,6 +3,8 @@ package myproject2;
 
 public class MyProject2 {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         System.out.println("Hello Java!");
+        
     }
 }
